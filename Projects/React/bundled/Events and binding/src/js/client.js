9bc0d609b609aa463eb1f2eb0f9fc45127cd1f64
Mike Twede <miketwede@gmail.com>
2017-08-25 16:55:55 -0700

    events and binding

---
+++ Projects/React/bundled/Events and binding/src/js/client.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./components/Layout";
+
+
+
+
+
+
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
